0cc7ea7328376330496dfa07d503aa40404e83ee
ebal5 <6240240+ebal5@users.noreply.github.com>
2024-04-19 15:26:19 +0900

    colorize unique each script

--- dot_scripts/executable_unique_each.sh
+++ dot_scripts/executable_unique_each.sh
@@ -5,10 +5,12 @@ tempA=$(mktemp)
 tempB=$(mktemp)
 
 # ユーザーからの入力を一時ファイルに保存
-echo "最初のテキストを入力してください (Ctrl-D で終了):"
+echo -e "\e[0;32m最初のテキストを入力してください (Ctrl-D で終了):\e[0m"
 cat > "$tempA"
-echo "次のテキストを入力してください (Ctrl-D で終了):"
+echo ""
+echo -e "\e[0;32m次のテキストを入力してください (Ctrl-D で終了):\e[0m"
 cat > "$tempB"
+echo ""
 
 # ソートして一時ファイルに保存
 sort "$tempA" > "${tempA}_sorted"
@@ -19,10 +21,12 @@ comm -23 "${tempA}_sorted" "${tempB}_sorted" > unique_to_A.txt
 comm -13 "${tempA}_sorted" "${tempB}_sorted" > unique_to_B.txt
 
 # 結果の出力
-echo "最初のテキストにのみ存在する行:"
+echo -e "\e[0;32m最初のテキストにのみ存在する行:\e[0m"
 cat unique_to_A.txt
-echo "次のテキストにのみ存在する行:"
+echo ""
+echo -e "\e[0;32m次のテキストにのみ存在する行:\e[0m"
 cat unique_to_B.txt
+echo ""
 
 # 一時ファイルの削除
 rm "$tempA" "$tempB" "${tempA}_sorted" "${tempB}_sorted" unique_to_A.txt unique_to_B.txt
