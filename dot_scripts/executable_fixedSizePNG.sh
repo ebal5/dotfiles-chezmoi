cc02b1b8f92d8501ab857ac848234e6597772d47
ebal5 <6240240+ebal5@users.noreply.github.com>
2024-08-15 01:09:27 +0900

    lint

--- dot_scripts/executable_fixedSizePNG.sh
+++ dot_scripts/executable_fixedSizePNG.sh
@@ -7,8 +7,8 @@ check() {
   exiftool >/dev/null || echo "Need to install exiftool" && exit 1
 }
 get_random_text() {
-  text=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w $1 | head -n 1)
-  echo $text
+  text=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w $1 | head -n 1)
+  echo "$text"
 }
 
 check
@@ -22,22 +22,22 @@ hex_color=$(hexdump -n 3 -v -e '"" 3/1 "%02X" "\n"' /dev/urandom)
 text=$(get_random_text $text_length)
 
 # フォントはどうでもいい
-convert -size ${img_size} xc:#${hex_color} \
+convert -size "${img_size}" "xc:#${hex_color}" \
   -gravity Center -pointsize 72 -annotate 0 "640x640" \
-  -gravity Center -pointsize 16 -annotate "+0+80" $text \
-  $img_name
+  -gravity Center -pointsize 16 -annotate "+0+80" "$text" \
+  "$img_name"
 
-img_file_size=$(wc -c <$img_name)
+img_file_size=$(wc -c <"$img_name")
 extra_text_size=$(($target_size - 21 - $img_file_size))
 
 if [ ${extra_text_size} -lt 1 ]; then
   echo too small target size
-  rm ${img_name}
+  rm "$img_name"
   exit 1
 fi
 
 extra_text_fn=$(mktemp)
-get_random_text $extra_text_size >${extra_text_fn}
+get_random_text $extra_text_size >"$extra_text_fn"
 
-exiftool -ignoreMinorErrors -overwrite_original ${img_name} -comment\<=${extra_text_fn} >/dev/null
-rm ${extra_text_fn}
+exiftool -ignoreMinorErrors -overwrite_original "$img_name" -comment\<="$extra_text_fn" >/dev/null
+rm "$extra_text_fn"
