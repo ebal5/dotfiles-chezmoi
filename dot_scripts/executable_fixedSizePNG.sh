5a1d5d13db02a7ebaa48080aee5710100e840e9e
ebal <6240240+ebal5@users.noreply.github.com>
2025-05-28 02:04:17 +0900

    Update dot_scripts/executable_fixedSizePNG.sh
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- dot_scripts/executable_fixedSizePNG.sh
+++ dot_scripts/executable_fixedSizePNG.sh
@@ -3,14 +3,14 @@
 # thanks to https://hostingstock.net/blog/20160206/
 
 check() {
-	if ! convert --version >/dev/null; then
-		echo "Need to install Imagemagick"
-		exit 1
-	fi
-	if ! exiftool >/dev/null; then
-		echo "Need to install exiftool"
-		exit 1
-	fi
+  if ! convert --version >/dev/null; then
+    echo "Need to install Imagemagick"
+    exit 1
+  fi
+  if ! exiftool >/dev/null; then
+    echo "Need to install exiftool"
+    exit 1
+  fi
 }
 get_random_text() {
 	text=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w "$1" | head -n 1)
